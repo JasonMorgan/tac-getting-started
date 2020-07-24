706063d5ec54a097093cf8e7773fed7d84587f53
Jason Morgan <jmorgan@f9vs.com>
2020-07-24 17:27:14 -0400

    Adding some app content.

--- create-an-app.sh
+++ create-an-app.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+## Change the image urls in your dockerfile to match the locally deployed harbor
+
+docker login my-registry-url
+
+docker build -t my-registry-url/tac/petclinic .
+
+docker push my-registry-url/tac/petclinic
